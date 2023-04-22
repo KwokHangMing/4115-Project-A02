97549cd04541d4f87ef3a3281b02d6b4fcd7ecbd
KwokHangMingSub <131445433+KwokHangMingSub@users.noreply.github.com>
2023-04-22 12:43:47 +0000

    deleted old migrations file.

--- migrations/env.py
+++ migrations/env.py
@@ -7,7 +7,6 @@ from flask import current_app
 
 from alembic import context
 from app.models import *
-
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
@@ -19,8 +18,8 @@ logger = logging.getLogger('alembic.env')
 
 # add your model's MetaData object here
 # for 'autogenerate' support
-
-# target_metadata = models.Base.metadata
+# from myapp import mymodel
+# target_metadata = mymodel.Base.metadata
 config.set_main_option(
     'sqlalchemy.url',
     str(current_app.extensions['migrate'].db.get_engine().url).replace(
